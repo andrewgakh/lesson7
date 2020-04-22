e036685bd7737692d39a4b57f8320e81f31c0b70
Andrew <andreyg44@gmail.com>
2020-04-22 23:32:44 +1000

    task 5

--- homework7.py
+++ homework7.py
@@ -1,3 +1,7 @@
+import csv
+import json
+import datetime
+import time
 from docxtpl import DocxTemplate
 
 # *********************************************************************************************************************
@@ -62,15 +66,70 @@ line1 = ['Toyota','Land Cruiser','25','4 000 000']
 line2 = ['Nissan', 'XTrail', '15', '3 000 000']
 name_ret = 'ООО Рога и копыта'
 
+print()
+print('Задача 3.')
+
+time_1 = time.time()    # start report
 gen_price(name_ret, line1, line2)
+time_2 = time.time()    # stop report
+delta_time = int((time_2 - time_1)*1000)
+
+print('Файл price-final.docx создан.')
+print('время затраченное на отчет', delta_time, ' мс')
 
 # *********************************************************************************************************************
 # Задача 4.
 # Создать csv файл с данными о машине.
 # *********************************************************************************************************************
 
+print()
+print('Задача 4.')
+car_list = [['Марка авто', 'Модель авто', 'Расход топлива л/100км', 'Стоимость руб.'],
+            ['Toyota','Land Cruiser','25','4 000 000'], ['Nissan', 'XTrail', '15', '3 000 000']]
+time_rep = ['Время создания отчета']
+time_1 = time.time()    # start report
+
+with open('car_price.csv', 'w') as f:
+    writer = csv.writer(f)
+    writer.writerows(car_list)
+
+time_2 = time.time()    # stop report
+delta_time = int((time_2 - time_1)*1000000)
+print(time_1)
+print(time_2)
+print(delta_time)
+time_rep.append(delta_time)
+time_rep.append('мкс')
+car_list.append(time_rep)
+
+with open('car_price.csv', 'w') as f:
+    writer = csv.writer(f)
+    writer.writerows(car_list)
+
+print('Запись в car_price.csv закончена.')
 
 # *********************************************************************************************************************
 # Задача 5.
 # Создать json файл с данными о машине.
 # *********************************************************************************************************************
+
+dict_cars_1 = {'brand':'Toyota', 'model':'Land Cruiser', 'raskhod':25, 'price':4000000,}
+# dict_cars_2 = {'brand':'Nissan', 'model':'XTrail', 'raskhod':15, 'price':3000000,}
+data_cars=[]
+
+with open('price_json.txt', 'w') as f:
+    json.dump(dict_cars_1, f)
+    # json.dump(dict_cars_2, f)
+
+print()
+print('Задача 5.')
+print('Запись в price_json.txt закончена.')
+
+print('Просмотр файла price_json.txt')
+print()
+with open('price_json.txt') as f:
+
+    # for i in f:
+    data_cars = json.load(f)
+    print(data_cars)
+
